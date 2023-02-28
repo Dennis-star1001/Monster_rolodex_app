2ac61f9dd5e77f06d45e61f994c6e03cf298463c
Dennis-star1001 <tdennis.developer@gmail.com>
2023-02-28 10:55:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component, React } from 'react'
+import * as ReactDOM from 'react-dom'
+import CardList from './components/card-list/card-list.component'
+class App extends Component {
+	constructor() {
+		super()
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		this.state = {
+			monsters: [],
+			searchField: '',
+		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((res) => res.json())
+			.then((users) =>
+				this.setState(
+					() => {
+						return {
+							monsters: users,
+						}
+					},
+					// () => {
+					// 	console.log(this.state)
+					// },
+				),
+			)
+	}
+	onSearchChange = (event) => {
+		const searchField = event.target.value.toLocaleLowerCase()
+
+		this.setState(() => {
+			return { searchField }
+		})
+	}
+	render() {
+		const { monsters, searchField } = this.state
+		const { onSearchChange } = this
+		// const monsterString = event.target.value.toLocaleLowerCase()
+		const filteredMonster = monsters.filter((monster) => {
+			return monster.name.toLocaleLowerCase().includes(searchField)
+		})
+
+		return (
+			<div className='App'>
+				<input
+					className='search-box'
+					type='search'
+					placeholder='search monsters'
+					onChange={onSearchChange}
+				/>
+				{/* {this.state.monsters.map((monster) => {
+					return <h1 key={monster.id}>{monster.name}</h1>
+				})} */}
+				<CardList monsters={filteredMonster} />
+			</div>
+		)
+	}
 }
-
-export default App;
+export default App
